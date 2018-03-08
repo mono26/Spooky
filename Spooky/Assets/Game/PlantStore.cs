f8c6ccfd8a7f2d1d51e07d7a73ad97415fd37f40
Desktop-Mono <nzubiria1@gmail.com>
2018-03-08 18:21:58 -0500

    Plant Store, Plant Point, Plant Blueprint. * Imported scripts from the old project. Commented functionality that doesn't work.

---
+++ Spooky/Assets/Game/PlantStore.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantStore : MonoBehaviour
+{
+    //Singleton part
+    private static PlantStore instance;
+    public static PlantStore Instance
+    {
+        get { return instance; }
+    }
+
+    //Esta es informacion para toda la programacion del plantpoint y sus funciones
+    public Transform buildUI;
+    public Transform plantUI;
+    public GameObject buildCanvas;
+    public GameObject plantCanvas;
+    public PlantPoint currentPlantPoint;
+
+    //public SceneFader sceneFader;
+
+    public AudioClip[] uiSounds;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;    //Parte del singleton en donde se asigna la unica instancia de la clase
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    //Metodos para el manejo de los plantpoints y la UI
+    public void SelectPlantPoint(PlantPoint plantPoint)     //Metodo que se llamara cada vez que el jugador haga click sobre un plant point.
+    {
+        if (currentPlantPoint == plantPoint)
+        {
+            DeselectPlantPoint();
+            return;
+        }
+        currentPlantPoint = plantPoint;
+        PlantStore.Instance.SetPlantPoint(currentPlantPoint);
+    }
+    public void SelectBuildPoint(PlantPoint plantPoint)     //Metodo que se llamara cada vez que el jugador haga click sobre un plant point.
+    {
+        if (currentPlantPoint == plantPoint)
+        {
+            DeselectBuildPoint();
+            return;
+        }
+        currentPlantPoint = plantPoint;
+        SetBuildPoint(currentPlantPoint);
+    }
+    public void DeselectPlantPoint()        //Function for deselection the plantpoint
+    {
+        currentPlantPoint = null;
+        HidePlantUI();
+    }
+    public void DeselectBuildPoint()        //Function for deselection the plantpoint
+    {
+        currentPlantPoint = null;
+        HideBuildUI();
+    }
+    public void SetPlantPoint(PlantPoint plantPoint)
+    {
+        //Si el plantPoint tiene una planta se activa el plantpointUI con la informacion de la planta.
+        currentPlantPoint = plantPoint;
+        plantUI.position = currentPlantPoint.transform.position;
+        plantCanvas.SetActive(true);
+    }
+    public void SetBuildPoint(PlantPoint plantPoint)
+    {
+        //Cuadno el plant poin esta vacio para sacar el buildUI
+        currentPlantPoint = plantPoint;
+        buildUI.position = currentPlantPoint.transform.position;
+        buildCanvas.SetActive(true);
+    }
+    public void HideBuildUI()
+    {
+        buildCanvas.SetActive(false);
+    }
+    public void HidePlantUI()
+    {
+        plantCanvas.SetActive(false);
+    }
+
+    //Esta parte del script esta dedicada a las funiones de la UI de las plantas. Tanto para la UI de la
+    //planta como el UI de construccion.
+    public void PurchasePlant(PlantBlueprint bluePrint)
+    {
+        if (LevelManager.Instance.currentMoney >= bluePrint.price)
+        {
+            currentPlantPoint.BuildPlant(bluePrint);
+            //SoundHandler.Instance.PlayClip(uiSounds[0]);
+            //SoundHandler.Instance.PlayClip(uiSounds[1]);
+            DeselectBuildPoint();
+        }
+        else return;
+    }
+    public void Upgrade()
+    {
+        if (LevelManager.Instance.currentMoney > currentPlantPoint.plantBluePrint.upgradePrice)
+        {
+            currentPlantPoint.UpgradePlant();
+            //SoundHandler.Instance.PlayClip(uiSounds[0]);
+        }
+        else return;
+    }
+    public void Sell()
+    {
+        currentPlantPoint.SellPlant();
+        //SoundHandler.Instance.PlayClip(uiSounds[0]);
+        DeselectPlantPoint();
+    }
+}
---
+++ Spooky/Assets/Plant Point/PlantPoint.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantPoint : MonoBehaviour
+{
+    public PlantBlueprint plantBluePrint;
+    public Plant currentPlant;
+
+    //public AudioClip[] plantPointSounds;
+
+    void Start()
+    {
+
+    }
+    public void OnClicked()
+    {
+        //Si ya hay una planta y un blueprint se selecciona el nodo
+        if (currentPlant && plantBluePrint)
+        {
+            PlantStore.Instance.SelectPlantPoint(this);
+            //Ejecutar codigo para poder subir de nivel o vender la torre
+        }
+        if (!currentPlant && !plantBluePrint)
+        {
+            PlantStore.Instance.SelectBuildPoint(this);
+            //Ejecutar codigo para que salga el canvas con los botones.
+        }
+    }
+    public void BuildPlant(PlantBlueprint blueprint)       //Luego de que se tenga una planta seleccionada cuando se escoja un nodo se construira ahi
+    {
+        //GameObject plant = PoolsManagerPlants.Instance.GetPlant(blueprint.plant.plantInfo.objectIndex);
+        //plant.transform.position = transform.position;
+        //currentPlant = plant.GetComponent<Plant>();
+        plantBluePrint = blueprint;
+        LevelManager.Instance.TakeMoney(blueprint.price);
+        //SoundHandler.Instance.PlayClip(plantPointSounds[0]);
+    }
+    public void SellPlant()
+    {
+        //GameManager.Instance.GiveMoney(currentPlant.GetComponent<AIPlantController>().plantInfo.plantReward);
+        plantBluePrint = null;
+        //PoolsManagerPlants.Instance.ReleasePlant(currentPlant.gameObject);
+        currentPlant = null;
+        PlantStore.Instance.DeselectPlantPoint();
+        //SoundHandler.Instance.PlayClip(plantPointSounds[1]);
+    }
+    public void UpgradePlant()
+    {
+        LevelManager.Instance.TakeMoney(plantBluePrint.upgradePrice);
+        //PoolsManagerPlants.Instance.ReleasePlant(currentPlant.gameObject);
+        //currentPlant = PoolsManagerPlants.Instance.GetPlant(plantBluePrint.upgradePlant.plantInfo.objectIndex).GetComponent<PlantController>();
+        currentPlant.transform.position = transform.position;
+        PlantStore.Instance.DeselectPlantPoint();
+        //SoundHandler.Instance.PlayClip(plantPointSounds[2]);
+    }
+    public void DestroyPlant()
+    {
+        plantBluePrint = null;
+        currentPlant = null;
+    }
+}
