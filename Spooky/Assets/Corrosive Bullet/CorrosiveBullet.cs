78bb4ef7065c428c55ef92d73ce92c41d801c24f
Desktop-Mono <nzubiria1@gmail.com>
2018-03-12 16:13:33 -0500

    Bullet pool for Corrosive y tomato bullet.

--- Spooky/Assets/Corrosive Bullet/CorrosiveBullet.cs
+++ Spooky/Assets/Corrosive Bullet/CorrosiveBullet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CorrosiveBullet : Bullet
+public class CorrosiveBullet : Bullet, ISpawnable<Bullet>
 {
     [SerializeField]
     private float startTime;
@@ -17,6 +17,10 @@ public class CorrosiveBullet : Bullet
     protected GameObject corrosiveTrigger;
 
     protected List<Enemy> enemyList;
+
+    [SerializeField]
+    private static List<Bullet> bulletList = new List<Bullet>();
+    public List<Bullet> Pool { get { return bulletList; } }
 
     protected new void Awake()
     {
@@ -42,6 +46,8 @@ public class CorrosiveBullet : Bullet
             }
             else yield return new WaitForSeconds(1 / corrosiveTickRate);
         }
+
+        ReleaseBullet(this);
     }
 
     private void AddEnemy(Enemy _enemy)
@@ -64,12 +70,55 @@ public class CorrosiveBullet : Bullet
         else return;
     }
 
+    public Bullet Spawn(Transform _position)
+    {
+        if (Pool.Count == 0)
+            AddToPool();
+        Bullet enemy = Pool[Pool.Count - 1];
+        Pool.RemoveAt(Pool.Count - 1);
+        SetBulletPosition(enemy, _position);
+        enemy.gameObject.SetActive(true);
+        return enemy;
+    }
+
+    private void AddToPool()
+    {
+        var parentPool = GameObject.Find("Bullets");    //Can't store a transform inside a prefab. Ensure always a tranform Enemies on level.
+        Bullet bullet = Instantiate(
+            gameObject,
+            parentPool.transform.position,
+            Quaternion.Euler(90f, 0f, 0f)
+            ).GetComponent<Bullet>();
+        bullet.gameObject.SetActive(false);
+        Pool.Add(bullet);
+    }
+
+    private void SetBulletPosition(Bullet _bullet, Transform target)
+    {
+        _bullet.transform.position = target.position;
+    }
+
+    public void ReleaseBullet(Bullet _bullet)
+    {
+        Restart(_bullet);
+        _bullet.gameObject.SetActive(false);
+        Pool.Add(_bullet);
+    }
+
+    private void Restart(Bullet _bullet)
+    {
+        _bullet.rigidBody.velocity = Vector3.zero;
+    }
+
     protected void OnCollisionEnter(Collision _collision)
     {
-        GetComponent<Collider>().enabled = false;   // So the only collidr activated is the trigger for the area damage.
-        rigidBody.velocity = Vector3.zero;
-        rigidBody.constraints = RigidbodyConstraints.FreezeRotationZ;   // TODO fin better way to do this.
-        StartCoroutine(CorrosiveEffect());
+        if (_collision.gameObject.CompareTag("Enemy"))
+        {
+            GetComponent<Collider>().enabled = false;   // So the only collidr activated is the trigger for the area damage.
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.constraints = RigidbodyConstraints.FreezeRotationZ;   // TODO fin better way to do this.
+            StartCoroutine(CorrosiveEffect());
+        }
     }
 
     protected void OnTriggerEnter(Collider _collider)
--- Spooky/Assets/Tomato Bullet/Tomato Bullet.cs
+++ Spooky/Assets/Tomato Bullet/Tomato Bullet.cs
@@ -1,16 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class TomatoBullet : MonoBehaviour {
+public class TomatoBullet : Bullet, ISpawnable<Bullet>
+{
+    [SerializeField]
+    private static List<Bullet> bulletList = new List<Bullet>();
+    public List<Bullet> Pool { get { return bulletList; } }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Bullet Spawn(Transform _position)
+    {
+        if (Pool.Count == 0)
+            AddToPool();
+        Bullet enemy = Pool[Pool.Count - 1];
+        Pool.RemoveAt(Pool.Count - 1);
+        SetBulletPosition(enemy, _position);
+        enemy.gameObject.SetActive(true);
+        return enemy;
+    }
+
+    private void AddToPool()
+    {
+        var parentPool = GameObject.Find("Bullets");    //Can't store a transform inside a prefab. Ensure always a tranform Enemies on level.
+        Bullet bullet = Instantiate(
+            gameObject,
+            parentPool.transform.position,
+            Quaternion.Euler(90f, 0f, 0f)
+            ).GetComponent<Bullet>();
+        bullet.gameObject.SetActive(false);
+        Pool.Add(bullet);
+    }
+
+    private void SetBulletPosition(Bullet _bullet, Transform target)
+    {
+        _bullet.transform.position = target.position;
+    }
+
+    public void ReleaseBullet(Bullet _bullet)
+    {
+        Restart(_bullet);
+        _bullet.gameObject.SetActive(false);
+        Pool.Add(_bullet);
+    }
+
+    private void Restart(Bullet _bullet)
+    {
+        _bullet.rigidBody.velocity = Vector3.zero;
+    }
+
+    protected void OnCollisionEnter(Collision _collision)
+    {
+        if (_collision.gameObject.CompareTag("Enemy"))
+            ReleaseBullet(this);
+    }
+
 }
