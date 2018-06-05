3110cc1024cca88194d526215bde867d1593fa28
Desktop-Mono <nzubiria1@gmail.com>
2018-06-04 20:31:04 -0500

    * random.

--- Spooky/Assets/Game/SoundManager.cs
+++ Spooky/Assets/Game/SoundManager.cs
@@ -1,20 +1,17 @@
 ﻿using UnityEngine;
 
 public class SoundManager : PersistenSingleton<SoundManager>
-{                //Drag a reference to the audio source which will play the sound effects.
+{
     [SerializeField]
-    private AudioSource musicSource;                 //Drag a reference to the audio source which will play the music. 
+    private AudioSource musicSource;
 
     public void PlaySfx(AudioSource _source , AudioClip _audioClip)
     {
-        // So we dont 
         if (_source.loop == true)
             _source.loop = false;
 
-        //Set the clip of our efxSource audio source to the clip passed in as a parameter.
         _source.clip = _audioClip;
 
-        //Play the clip.
         _source.Play();
         //efxSource.PlayOneShot(_audioClip, 0.2f);
     }
